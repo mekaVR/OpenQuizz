9fb9c51cfb229ef06bdf696d3edac97898c705a0
meka VR <candau.herbert@gmail.com>
2018-03-26 17:13:12 +0200

    create Game class, structure Question and add QuestionManager file for get Question via Open Trivia

--- OpenQuizz/Model/Game.swift
+++ OpenQuizz/Model/Game.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+class Game {
+    var score = 0
+    
+    private var questions = [Question]()
+    private var currentIndex = 0
+    
+    var state:State = .ongoing
+    
+    enum State {
+        case ongoing, over
+    }
+    
+    private func receiveQuestions(_ questions: [Question]){
+        self.questions = questions
+        state = .ongoing
+    }
+    
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+    
+    func refresh() {
+        score = 0
+        state = .over
+        currentIndex = 0
+        
+        QuestionManager.shared.get(completionHandler: receiveQuestions)
+    }
+    
+    func answerCurrentQuestion(with answer: Bool){
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer){
+            score += 1
+        }
+        goToNextQuestion()
+    }
+    
+    func goToNextQuestion() {
+        if currentIndex < questions.count-1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    
+    func finishGame() {
+        state = .over
+    }
+}

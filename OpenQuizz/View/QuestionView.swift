261e9d037b80afe7fdc6fe673d78f5ecb2e3b289
meka VR <candau.herbert@gmail.com>
2018-03-26 22:46:41 +0200

    create custom view: QuestionView
    connect two component label and icon

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by meka on 26/03/2018.
+//  Copyright © 2018 meka. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
+
+}
